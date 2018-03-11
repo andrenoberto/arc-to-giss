fc181c1ff0250fe6766486abf2d5ddd8b873b3e7
Andre Noberto <joao.andre.souza@accenture.com>
2018-03-11 18:28:31 -0300

    Refactoring code

--- app.ts
+++ app.ts
@@ -2,21 +2,26 @@ import * as JsonFile from 'jsonfile';
 import * as Jetty from 'jetty';
 import {GeoRoute} from './GeoRoute';
 
-// Let's get our input data
+// Let's arrange
+let obj: any;
 const data = require(process.env.npm_package_config_input || './example.json');
 const geoRoute = new GeoRoute(data);
-
-// This package will extend our console.log features
 let jetty = new Jetty(process.stdout);
-jetty.clear();
 
-//Let's process our features and obtain the results
-geoRoute.processFeaturesFromData();
-const obj = geoRoute.getFeatures();
+//Let's start processing our data and obtain the results
+jetty.clear();
+switch (process.env.npm_package_config_algorithm) {
+    case 'GeoRoute':
+        geoRoute.processFeaturesFromData();
+        obj = geoRoute.getFeatures();
+        break;
+}
 
 // Finally, let's save our result into a file
-jetty.text('\nWriting process started...');
-const file = process.env.npm_package_config_output || './output_result.json';
-JsonFile.writeFile(file, obj, {spaces: 2}, (err) => {
-    jetty.text(err ? err : '\nOutput file has been successfully written.');
-});
+if (obj) {
+    jetty.text('\nWriting process started...');
+    const file = process.env.npm_package_config_output || './output_result.json';
+    JsonFile.writeFile(file, obj, {spaces: 2}, (err) => {
+        jetty.text(err ? err : '\nOutput file has been successfully written.');
+    });
+}
