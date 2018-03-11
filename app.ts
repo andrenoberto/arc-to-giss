e4c9a6efe86015a900ce177c43dc5e765384bb31
Andre Noberto <joao.andre.souza@accenture.com>
2018-03-11 18:54:43 -0300

    Reordering vars

--- app.ts
+++ app.ts
@@ -5,13 +5,13 @@ import {GeoRoute} from './GeoRoute';
 // Let's arrange
 let obj: any;
 const data = require(process.env.npm_package_config_input || './example.json');
-const geoRoute = new GeoRoute(data);
-let jetty = new Jetty(process.stdout);
+const jetty = new Jetty(process.stdout);
 
 //Let's start processing our data and obtain the results
 jetty.clear();
 switch (process.env.npm_package_config_algorithm) {
     case 'GeoRoute':
+        const geoRoute = new GeoRoute(data);
         geoRoute.processFeaturesFromData();
         obj = geoRoute.getFeatures();
         break;
